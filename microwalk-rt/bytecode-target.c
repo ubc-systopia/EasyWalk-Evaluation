67928874507917b597bca56f7b48b1c6be473c10
Kjell Dankert <kdankert@cs.ubc.ca>
2026-03-19 01:48:27 -0700

    Fix test setup

--- microwalk-rt/bytecode-target.c
+++ microwalk-rt/bytecode-target.c
@@ -4,34 +4,50 @@
 
 #include "wrapper.h"
 
-JSRuntime *rtm = NULL;
+JSRuntime *rt = NULL;
 JSContext *ctx = NULL;
-JSValue global_obj = {};
 JSValue fn = {};
 
-int js_std_eval_file_global(JSContext *ctx, const char *filename) {
+JSValue js_std_eval_file_global(JSContext *ctx, const char *filename) {
     size_t buf_len = 0;
     uint8_t *buf = js_load_file(ctx, &buf_len, filename);
-    const JSValue val = JS_Eval(ctx, (const char *) buf, buf_len, filename, JS_EVAL_TYPE_GLOBAL);
-    if (JS_IsException(val)) {
+    const JSValue module = JS_Eval(ctx, (const char *) buf, buf_len, filename, JS_EVAL_TYPE_MODULE | JS_EVAL_FLAG_COMPILE_ONLY);
+    if (JS_IsException(module)) {
         js_std_dump_error(ctx);
-        return -1;
+        exit(-1);
     }
-    JS_FreeValue(ctx, val);
+    js_module_set_import_meta(ctx, module, 1, 1);
+    const JSValue promise = JS_EvalFunction(ctx, module);
+    if (JS_IsException(promise)) {
+        js_std_dump_error(ctx);
+        exit(-1);
+    }
+    const JSValue res = js_std_await(ctx, promise);
+    if (JS_IsException(res)) {
+        js_std_dump_error(ctx);
+        exit(-1);
+    }
+    JS_FreeValue(ctx, res);
     js_free(ctx, buf);
-    return 0;
+    return module;
 }
 
 void init_target(const char *file) {
-    rtm = JS_NewRuntime();
+    rt = JS_NewRuntime();
     js_std_set_worker_new_context_func(JS_NewContext);
-    js_std_init_handlers(rtm);
-    ctx = JS_NewContext(rtm);
-    JS_SetModuleLoaderFunc2(rtm, NULL, js_module_loader, js_module_check_attributes, NULL);
+    js_std_init_handlers(rt);
+    ctx = JS_NewContext(rt);
+    JS_SetModuleLoaderFunc2(rt, NULL, js_module_loader, js_module_check_attributes, NULL);
+    js_std_add_helpers(ctx, -1, NULL);
 
-    js_std_eval_file_global(ctx, file);
-    global_obj = JS_GetGlobalObject(ctx);
-    fn = JS_GetPropertyStr(ctx, global_obj, "processTestcase");
+    const JSValue module = js_std_eval_file_global(ctx, file);
+    if (JS_VALUE_GET_TAG(module) != JS_TAG_MODULE) {
+        exit(-1);
+    }
+    JSModuleDef *m = JS_VALUE_GET_PTR(module);
+    const JSValue ns = JS_GetModuleNamespace(ctx, m);
+    fn = JS_GetPropertyStr(ctx, ns, "processTestcase");
+    JS_FreeValue(ctx, ns);
 }
 
 void init_target_run() {
@@ -43,14 +59,13 @@ void target_run(const char *buffer, const size_t buffer_size) {
         mw_exit_error("Buffer size must be non-empty");
     }
 
-    char *end_ptr = NULL;
-    const int input = (int) strtol(buffer, &end_ptr, 10);
-    if (end_ptr == NULL) {
-        mw_exit_error("Integer conversion from testcase failed");
+    JSValue arg = JS_NewArrayBufferCopy(ctx, (const uint8_t *)buffer, buffer_size);
+    const JSValue res = JS_Call(ctx, fn, JS_UNDEFINED, 1, &arg);
+    if (JS_IsException(res)) {
+        js_std_dump_error(ctx);
+        exit(-2);
     }
-
-    JSValue arg = JS_NewInt32(ctx, input);
-    JS_Call(ctx, fn, global_obj, 1, &arg);
+    JS_FreeValue(ctx, res);
     JS_FreeValue(ctx, arg);
 }
 
@@ -72,9 +87,7 @@ void cleanup_target_filter(void) {}
 
 void cleanup_target(void) {
     JS_FreeValue(ctx, fn);
-    JS_FreeValue(ctx, global_obj);
-
-    js_std_free_handlers(rtm);
+    js_std_free_handlers(rt);
     JS_FreeContext(ctx);
-    JS_FreeRuntime(rtm);
+    JS_FreeRuntime(rt);
 }
